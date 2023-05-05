f9805ced86dcc3571a2de08867aaaacc26b97514
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-05 12:17:54 +0200

    onwords to state

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -5,10 +5,20 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.colorOne, this.colorTwo, {super.key});
+  GradientContainer(this.colorOne, this.colorTwo, {super.key});
+
+  GradientContainer.purple({super.key})
+      : colorOne = Colors.purple,
+        colorTwo = Colors.indigo;
 
   final Color colorOne;
   final Color colorTwo;
+  var activeDiceImage = 'assets/images/dice-1.png';
+
+  void rollDice() {
+    print("changed dice");
+    activeDiceImage = 'assets/images/dice-1.png';
+  }
 
   @override
   Widget build(context) {
@@ -18,7 +28,25 @@ class GradientContainer extends StatelessWidget {
               colors: [colorOne, colorTwo],
               begin: startAlignment,
               end: endAlignment)),
-      child: const Center(child: StyledText("Bonjou'")),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Image.asset(
+            'assets/images/dice-1.png',
+            width: 200,
+          )),
+          const SizedBox(width: 50),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+                padding: const EdgeInsets.all(5),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 20)),
+            child: const Text("Press"),
+          )
+        ],
+      ),
     );
   }
 }
