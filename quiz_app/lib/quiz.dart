8c73b408dd4870976fe173a4637eba4b8ce5d83f
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-06 09:44:18 +0200

    lifted state up and added extra components

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -11,17 +12,28 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  String activeScreen = "start-screen";
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = "question-screen";
+    });
+  }
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 28, 1, 33),
-              Color.fromARGB(255, 86, 21, 99),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-            child: const StartScreen()),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 28, 1, 33),
+            Color.fromARGB(255, 86, 21, 99),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: activeScreen == "start-screen"
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
+        ),
       ),
     );
   }
