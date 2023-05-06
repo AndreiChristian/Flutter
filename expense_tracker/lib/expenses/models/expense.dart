16336887399d987dac57548ea305bcd828dab948
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-06 17:53:06 +0200

    building a more complex user interface

--- expense_tracker/lib/expenses/models/expense.dart
+++ expense_tracker/lib/expenses/models/expense.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.food_bank_sharp,
+  Category.leisure: Icons.movie_filter,
+  Category.work: Icons.work,
+  Category.travel: Icons.flight_takeoff,
+};
 
 class Expense {
   Expense(
@@ -17,4 +25,10 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    var day = date.toLocal().toString().split(" ")[0];
+    var fday = day.split("-");
+    return fday.join("/");
+  }
 }
--- expense_tracker/lib/expenses/widgets/expenses_list.dart
+++ expense_tracker/lib/expenses/widgets/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/expenses/models/expense.dart';
+import 'package:expense_tracker/expenses/widgets/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -9,8 +10,7 @@ class ExpensesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Text(expenses[index].title),
-    );
+        itemCount: expenses.length,
+        itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]));
   }
 }
