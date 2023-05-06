9d131f3844257d795af6a89532e0bfc48325c79a
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-06 12:12:05 +0200

    bonjour

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -3,36 +3,55 @@ import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() => _QuestionsScreenState();
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentIndex = 0;
+
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
+    setState(() {
+      currentIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final CurrentQuestion = questions[0];
+    final currentQuestion = questions[currentIndex];
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            CurrentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 30,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...CurrentQuestion.answers.map((answer) {
-            return AnswerButton(answer, () {});
-          }),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answer, () {
+                answerQuestion(answer);
+              });
+            }),
+          ],
+        ),
       ),
     );
   }
--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,6 +1,7 @@
+import 'package:quiz_app/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
-import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -12,12 +13,25 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+
   String activeScreen = "start-screen";
 
   void switchScreen() {
     setState(() {
       activeScreen = "question-screen";
     });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = "start-screen";
+      });
+    }
   }
 
   @override
@@ -32,7 +46,9 @@ class _QuizState extends State<Quiz> {
           ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
           child: activeScreen == "start-screen"
               ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+              : QuestionsScreen(
+                  onSelectAnswer: chooseAnswer,
+                ),
         ),
       ),
     );
