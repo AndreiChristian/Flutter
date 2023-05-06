38affa4e2e2e2339bc917b86aff359ddfb8663ef
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-06 08:34:29 +0200

    finished screen

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: StartScreen(),
+      body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 28, 1, 33),
+            Color.fromARGB(255, 86, 21, 99),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: const StartScreen()),
     ),
   ));
 }
--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -5,6 +5,33 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          "assets/images/quiz-logo.png",
+          width: 300,
+          color: const Color.fromARGB(136, 255, 255, 255),
+        ),
+        const SizedBox(
+          height: 80,
+        ),
+        const Text(
+          "Flutter Quiz",
+          style: TextStyle(
+              color: Color.fromARGB(255, 232, 175, 195), fontSize: 40),
+        ),
+        const SizedBox(
+          width: 100,
+        ),
+        OutlinedButton.icon(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+          label: const Text("Start the quiz"),
+          icon: const Icon(Icons.arrow_right_alt),
+        )
+      ],
+    ));
   }
 }
