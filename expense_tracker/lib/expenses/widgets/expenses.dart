d464658543db62de140d982563e668577d169b84
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-06 17:07:26 +0200

    code splitting and file structure

---
+++ expense_tracker/lib/expenses/widgets/expenses.dart
@@ -0,0 +1,39 @@
+import 'package:expense_tracker/expenses/widgets/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        amount: 19.99,
+        date: DateTime.now(),
+        title: "Flutter Course",
+        category: Category.work),
+    Expense(
+        amount: 16.99,
+        date: DateTime.now(),
+        title: "Cinema",
+        category: Category.leisure)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          const Text('data'),
+        ],
+      ),
+    );
+  }
+}
