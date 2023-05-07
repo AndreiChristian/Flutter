bd9b0231f683f4486305597e516ca55d95d627ee
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-07 12:26:41 +0200

    added grid view

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/categories.dart';
+import 'package:todo_app/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
---
+++ todo_app/lib/widgets/category_grid_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/models/category.dart' as category_model;
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final category_model.Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+              colors: [category.color.withOpacity(0.55), category.color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
+  }
+}
