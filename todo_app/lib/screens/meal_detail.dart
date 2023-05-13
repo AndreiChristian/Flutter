1b17b7061655fbdbb7042c9ed6f339b34fd42dc4
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-13 13:02:24 +0200

    hello again

--- todo_app/lib/screens/meal_detail.dart
+++ todo_app/lib/screens/meal_detail.dart
@@ -12,9 +12,43 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Center(
-        child: Image(
-          image: NetworkImage(meal.imageUrl),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Text(
+              "Ingredients",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(),
+              ),
+            const SizedBox(
+              height: 24,
+            ),
+            for (final ingredient in meal.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Text(
+                  ingredient,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(),
+                ),
+              ),
+          ],
         ),
       ),
     );
